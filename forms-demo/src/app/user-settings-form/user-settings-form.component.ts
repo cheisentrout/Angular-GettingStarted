f7986314c70105eebbe0f43b810382324864cd96
Clare Eisentrout <ceisentrout@predictiveindex.com>
2021-07-25 21:10:29 -0400

    Angular form validation

--- forms-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ forms-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -10,13 +11,16 @@ export class UserSettingsFormComponent implements OnInit {
 
   // This class property adheres to the custom type of the UserSettings interface!
   originalUserSettings: UserSettings = {
-    name: "Clare",
-    emailOffers: true,
-    interfaceStyle: 'dark',
-    subscriptionType: 'Annual',
-    notes: "some test notes!"
+    name: null,
+    emailOffers: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null
   }
 
+  /* Creating an original version of the user data, and then the typical version below,
+  is one way to protect data in the case that a form is deleted, refreshed, or somehow loses
+  its data before it's submitted. */
   userSettings: UserSettings = { ...this.originalUserSettings }
 
   constructor() { }
@@ -24,4 +28,12 @@ export class UserSettingsFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onBlur(field: NgModel) {
+    console.log("In onBlur: ", field.valid);
+    
+  }
+
+  onSubmit(form: NgForm) {
+    console.log("in onSubmit: ", form.valid);
+  }
 }
