53b4d0d5622a13e38d2ad44aea55a2150da773b2
Clare Eisentrout <ceisentrout@predictiveindex.com>
2021-07-27 12:05:12 -0400

    Added basic routing

--- angular-routing/src/app/app-routing.module.ts
+++ angular-routing/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
+/*==========================================
+ANGULAR MODULES
+===========================================*/
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+/*==========================================
+CUSTOM COMPONENTS
+===========================================*/
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomerComponent } from './customer/customer.component';
+
+// Here is where you add all routes for an application:
+// - use Angular Essentials shortcuts (a-path) to add paths
+// the path key links to a string of what will appear in the URL
+// the component key tells the path which component to render at that endpoint
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'customer', component: CustomerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
